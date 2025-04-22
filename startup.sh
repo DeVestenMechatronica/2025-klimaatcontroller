e7ea84eff91d60ac2df51dcee2804305c9644bd0
RRCH32 <nayomivelders7@gmail.com>
2025-04-22 11:33:58 +0100

    Startup script: inline documentatie

--- startup.sh
+++ startup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash 
 
+#   startup.sh
+#   Dit script start ons Python programma. Dit script wordt
+#   gebruikt door de systemd service
+
+# We starten de virual environment voor Python
 source /home/mechatronica/klimaatcontroller/venv/bin/activate
+# We starten ons Python programma
 python /home/mechatronica/klimaatcontroller/project.py
