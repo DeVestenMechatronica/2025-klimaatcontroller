12cf57c6010794af50f337724712fd248d1268d8
RRCH32 <nayomivelders7@gmail.com>
2025-04-22 09:23:13 +0100

    Update ventilator ON/OFF DIctated by TEMP

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ hier kan je uitleggen hoe je het project installeert
 - De [Edgeberry](https://github.com/Edgeberry) repository
 - De [Edgeberry Sense'n'Drive Cartridge](https://github.com/Edgeberry/Edgeberry_SenseAndDrive_Cartridge)
 - De [Freya](https://github.com/Freya-Vivariums/Freya-sensor) sensor
-- De [cmds]https://learn.pimoroni.com/article/getting-started-with-bme680-breakout voor de sensor
+- De [cmds](https://learn.pimoroni.com/article/getting-started-with-bme680-breakout) voor de sensor
--- project.py
+++ project.py
@@ -1,33 +1,43 @@
-#from gpiozero import DigitalOutputDevice
+from gpiozero import DigitalOutputDevice
 import bme680
 import time
 from time import sleep
 
+TEMPERATURE_HIGH = 23.3
+TEMPERATURE_LOW = 22.5 
+
 # Create a sensor object
 sensor = bme680.BME680()
 
-print("Hello world")
-#ventilator=DigitalOutputDevice(21)
-#while(False): #timed out
-    #ventilator.toggle()
-    #sleep(5)
+sensor.set_humidity_oversample(bme680.OS_2X)
+sensor.set_pressure_oversample(bme680.OS_4X)
+sensor.set_temperature_oversample(bme680.OS_8X)
+sensor.set_filter(bme680.FILTER_SIZE_3)
+
+sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+sensor.set_gas_heater_temperature(320)
+sensor.set_gas_heater_duration(150)
+sensor.select_gas_heater_profile(0)
+
+ventilator=DigitalOutputDevice(12)
     
 
 
 
 # Function to read and print sensor values
-def read_bme680():
-    # Wait for new data to be available
-    if sensor.get_sensor_data():
-        print("Temperature: {:.2f} C".format(sensor.temperature))
-        print("Humidity: {:.2f} %".format(sensor.humidity))
-        print("Pressure: {:.2f} hPa".format(sensor.pressure))
-        print("Gas resistance: {:.2f} Ohms".format(sensor.gas_resistance))
-    else:
-        print("Failed to read sensor data.")
-
-# Continuously read and print data from the sensor
 while True:
-    read_bme680()
-    time.sleep(1)  # Wait 1 second before reading again
+   # ventilator.toggle()
+    if sensor.get_sensor_data():
+        output = "{0:.2f} C,{1:.2f} hPa,{2:.2f} %RH".format(sensor.data.temperature, sensor.data.pressure, sensor.data.humidity)
+
+        if sensor.data.heat_stable:
+            print("{0},{1} Ohms".format(output, sensor.data.gas_resistance))
+
+        else:
+            print(output)
+    if float(sensor.data.temperature) >= TEMPERATURE_HIGH:
+        ventilator.on()
+    elif float(sensor.data.temperature) <= TEMPERATURE_LOW:
+        ventilator.off()
+    sleep(5)
 
